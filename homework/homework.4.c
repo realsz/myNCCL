1e83573429a94fb41f6bcb59e0bd3912c2a90dcd
xp-pc <shang115@sina.cn>
2012-11-30 16:17:27 +0800

    modified homework.4.c

--- homework/homework.4.c
+++ homework/homework.4.c
@@ -33,3 +33,39 @@ int main(void)
 		
 	return 0;
 }
+
+/*use rand() function guess again*/
+/*use this rand() fail , google this url http://www.cnblogs.com/ngnetboy/archive/2012/11/23/2784078.html*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+
+{
+	int num_a,num_b;
+	
+	num_a = rand();
+	
+	printf("num_a is %d\n", num_a);
+	
+	printf("Input the num_b number:");
+	scanf("%d",&num_b);
+	
+	while ( num_a != num_b)
+	{
+		if ( num_a > num_b)
+		{
+			printf("num_b %d is less than num_a,pls input again", num_b);
+			scanf("%d", &num_b);
+		}
+		else{
+			printf("num_b %d is more than num_a,pls input again", num_b);
+			scanf("%d", &num_b);
+		
+		}
+			
+	}
+	
+	return 0;
+}
