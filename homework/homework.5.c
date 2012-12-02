42697f016b5f6d7a2d33a4fcc8dc8a4526e5ca57
testcc <shang301@126.com>
2012-12-02 23:59:59 +0800

    add homework.5.c -2--fail question

--- homework/homework.5.c
+++ homework/homework.5.c
@@ -33,3 +33,50 @@ int main(void)
     return 0;
 
 }
+
+/*find the max number*/
+
+#include <stdio.h>
+
+#define PRINT(x)   printf("--> %s--line-%d-- "#x" is %d\n",__FUNCTION__,__LINE__,x)
+//#define PRINT(x)
+int main(void)
+
+{
+	int i;
+	int temp_num,num_i;
+	int max_num;
+	
+	max_num=temp_num=num_i = 0;
+	
+	for(i = 0; i < 5; i++)
+	{
+	
+		temp_num = num_i;
+		
+		
+		printf("Pls input the num_i %d times\n", i + 1);
+		scanf("%d",&num_i);	
+		
+		
+		
+		PRINT(num_i);
+		PRINT(temp_num);
+	
+		
+		if ( temp_num >= num_i)		
+			max_num = temp_num;
+		else
+		{	
+			max_num = num_i;
+		}	
+		
+		PRINT(max_num);	
+	}
+	
+	printf("the max num is %d\n", max_num);
+	
+	return 0;
+}
+
+
